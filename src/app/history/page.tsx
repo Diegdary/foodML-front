ccaff46d9adb748a4d613e201d33eac3274f897c
Diegdary <diegojose3019@gmail.com>
2025-09-23 06:05:59 -0500

    history page finished, still missing colors

--- src/app/history/page.tsx
+++ src/app/history/page.tsx
@@ -1,9 +1,46 @@
 "use client";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar } from "@fortawesome/free-solid-svg-icons";
+import { faStar as voidStar } from "@fortawesome/free-regular-svg-icons";
 import { useState } from "react";
 
 export default function History() {//get info
   const [name, setName] = useState("Diego");
+  const [ratingsPack,setRatingsPack] = useState([{name:"Steak",rating:2.5,date:"15/07/2025",imgURL:"https:"},{name:"Rice and eggs",rating:5,date:"11/07/2024",imgURL:"https:"},{name:"Rice and eggs",rating:5,date:"11/07/2024",imgURL:"https:"},{name:"Rice and eggs",rating:5,date:"11/07/2024",imgURL:"https:"},{name:"Rice and eggs",rating:5,date:"11/07/2024",imgURL:"https:"}]);
+
+  const [foodPacks, setPacks] = useState([{pack:
+    [{ name: "Fishes", category: 'Salad', ingredients: ["Salt", "Lettuce", "Etc."] },
+    { name: "Taco", category: 'Spicy', ingredients: ["Taco", "Lettuce", "meat", "Lettuce", "meat", "Lettuce", "meat", "Lettuce", "meat", "Lettuce", "meat"
+    , "Lettuce", "meat", "Lettuce", "meat", "Lettuce", "meat", "Lettuce", "meat"] },
+    { name: "Fish", category: 'Fast food', ingredients: ["Salt", "Lots of lettuce", "Etc."] },
+    { name: "Lemon Soup", category: 'Soup', ingredients: ["Salt", "Watta", "Lemon"] }],date:"14/07/2025"},
+  {pack:[{ name: "Fishes", category: 'Salad', ingredients: ["Salt", "Lettuce", "Etc."] },
+    { name: "Taco", category: 'Spicy', ingredients: ["Taco", "Lettuce", "meat", "Lettuce", "meat", "Lettuce", "meat", "Lettuce", "meat", "Lettuce", "meat"
+    , "Lettuce", "meat", "Lettuce", "meat", "Lettuce", "meat", "Lettuce", "meat"] },
+    { name: "Fish", category: 'Fast food', ingredients: ["Salt", "Lots of lettuce", "Etc."] },
+    { name: "Lemon Soup", category: 'Soup', ingredients: ["Salt", "Watta", "Lemon"] }],date:"13/07/2025"},
+  {pack:[{ name: "Fishes", category: 'Salad', ingredients: ["Salt", "Lettuce", "Etc."] },
+    { name: "Taco", category: 'Spicy', ingredients: ["Taco", "Lettuce", "meat", "Lettuce", "meat", "Lettuce", "meat", "Lettuce", "meat", "Lettuce", "meat"
+    , "Lettuce", "meat", "Lettuce", "meat", "Lettuce", "meat", "Lettuce", "meat"] },
+    { name: "Fish", category: 'Fast food', ingredients: ["Salt", "Lots of lettuce", "Etc."] },
+    { name: "Lemon Soup", category: 'Soup', ingredients: ["Salt", "Watta", "Lemon"] }],date:"12/07/2025"},
+  {pack:[{ name: "Fishes", category: 'Salad', ingredients: ["Salt", "Lettuce", "Etc."] },
+    { name: "Taco", category: 'Spicy', ingredients: ["Taco", "Lettuce", "meat", "Lettuce", "meat", "Lettuce", "meat", "Lettuce", "meat", "Lettuce", "meat"
+    , "Lettuce", "meat", "Lettuce", "meat", "Lettuce", "meat", "Lettuce", "meat"] },
+    { name: "Fish", category: 'Fast food', ingredients: ["Salt", "Lots of lettuce", "Etc."] },
+    { name: "Lemon Soup", category: 'Soup', ingredients: ["Salt", "Watta", "Lemon"] }],date:"11/07/2024"},
+  ]);
+
+    const starsController = (position:number)=>{
+    const follow_up = [0,0,0,0,0].map((_element,index)=>index<=position?1:0);
+    return follow_up.map((element,index)=>
+    <FontAwesomeIcon key={index} icon={faStar} className={element==1?'text-yellow-500':'text-white'} size='1x'/>
+    );
+  }
+
+
+
     return (
       <div>
         <header className="h-14 w-full border border-black border-solid">
@@ -22,13 +59,42 @@ export default function History() {//get info
           </nav>
         </header>
         <main className="flex flex-col items-center">
-          <h2 className="text-left">Previous recomendations:</h2>
-          <article className="w-[90%] h-[330px] border border-solid border-black rounded-md ">
-
+          <h2 className="mx-5 my-5 text-left text-xl font-bold">Previous recomendations:</h2>
+          <article className="bg-amber-100 w-[90%] h-[330px] p-5 border border-solid border-black rounded-md flex gap-10 overflow-x-auto overflow-y-hidden" style={{scrollbarColor:"#8b8b8b #fef3c6"}}>
+          {foodPacks.map((value,index)=>
+          <div key={index} className="w-[calc((100%-120px)/4)] flex flex-col justify-center shrink-0">
+            <figure className="bg-[#0002]  p-4.5 border border-black border-solid w-full h-[70%] flex flex-col justify-between items-start rounded-md overflow-hidden">
+                <div>
+                  <ul className="list-disc ps-5">
+                    {value.pack.map((currentFood,foodIndex)=>
+                    <li key={foodIndex}>{currentFood.name}</li>
+                    )}
+                  </ul>
+                </div>
+                <div className="w-full">
+                  <h3 className="font-bold text-lg">Pack {index+1}</h3>
+                </div>
+              </figure>
+              <p className="ml-1">{value.date}</p>
+          </div>
+          )}
           </article>
-          <h2 className="text-left">Your ratings:</h2>
-          <article className="w-[90%] h-[330px] border border-solid border-black rounded-md ">
-
+          <h2 className="mx-5 my-5 text-left text-xl font-bold">Your ratings:</h2>
+          <article className="bg-amber-100 w-[90%] h-[330px] p-5 border border-solid border-black rounded-md flex gap-10 overflow-x-auto overflow-y-hidden" style={{scrollbarColor:"#8b8b8b #fef3c6"}}>
+            {ratingsPack.map((value,index)=>
+            <div key={index} className="w-[calc((100%-120px)/4)] flex flex-col justify-center shrink-0">
+              <figure className="z-10 relative border border-black border-solid w-full h-[70%] flex flex-col justify-between items-start rounded-md overflow-hidden">
+                <img className="-z-10 absolute w-full h-full object-cover" src="./template.jpeg"/>
+                <div className="w-full z-50 p-2 bg-linear-to-b from-[rgba(0,0,0,6)] to-[rgba(0,0,0,0)]">
+                  <h3 className="text-xl font-bold text-white">{value.name}</h3>
+                </div>
+                <div className="w-full z-50 p-2 bg-linear-to-b from-[rgba(0,0,0,0)] to-[rgba(0,0,0,6)]">
+                  {starsController(value.rating-1)}
+                </div>
+              </figure>
+              <p>{value.date}</p>
+            </div>
+            )}
           </article>
         </main>
             
--- src/app/home-page.tsx
+++ src/app/home-page.tsx
@@ -7,11 +7,16 @@ import { useState } from "react";
 
 
 export default function Home() {
-  const [name, setName] = useState("Diego");
+  const [name, setName] = useState("Diego Jose");
   const [Findex, setFindex] = useState(0);
   const [temp_rating, setTempRt] = useState([0,-1]);//[old position, current position]
-  const [foodPack, setPack] = useState([{ name: "Fishes", category: 'Salad', ingredients: ["Salt", "Lettuce", "Etc."] }, { name: "Taco", category: 'Spicy', ingredients: ["Taco", "Lettuce", "meat", "Lettuce", "meat", "Lettuce", "meat", "Lettuce", "meat", "Lettuce", "meat"
-    , "Lettuce", "meat", "Lettuce", "meat", "Lettuce", "meat", "Lettuce", "meat"] }, { name: "Fish", category: 'Fast food', ingredients: ["Salt", "Lots of lettuce", "Etc."] }, { name: "Lemon Soup", category: 'Soup', ingredients: ["Salt", "Watta", "Lemon"] }]);
+  const [foodPack, setPack] = useState([
+    { name: "Fishes", category: 'Salad', ingredients: ["Salt", "Lettuce", "Etc."] },
+    { name: "Taco", category: 'Spicy', ingredients: ["Taco", "Lettuce", "meat", "Lettuce", "meat", "Lettuce", "meat", "Lettuce", "meat", "Lettuce", "meat"
+    , "Lettuce", "meat", "Lettuce", "meat", "Lettuce", "meat", "Lettuce", "meat"] },
+    { name: "Fish", category: 'Fast food', ingredients: ["Salt", "Lots of lettuce", "Etc."] },
+    { name: "Lemon Soup", category: 'Soup', ingredients: ["Salt", "Watta", "Lemon"] },
+  { name: "Lemon Soup", category: 'Soup', ingredients: ["Salt", "Watta", "Lemon"] }]);
 
   const starsController = (position:number)=>{
     const follow_up = [0,0,0,0,0].map((_element,index)=>index<=position?1:0);
@@ -23,6 +28,10 @@ export default function Home() {
 
   const starClickController = (position:number)=>{
     setTempRt([position,position]);
+  }
+
+  const getFirstName = (fullName:string)=>{
+    return fullName.split(' ')[0];
   }
   
 
@@ -44,10 +53,10 @@ export default function Home() {
         </nav>
       </header>
       <main className="flex flex-col items-center">
-        <h1 className="self-start mx-5 my-7 text-xl">Hello {name}. Are any of these of interest to you?</h1>
-        <article className="bg-amber-100 border border-black border-solid w-[90%] h-[37vh] p-5 rounded-md flex gap-10">
+        <h1 className="self-start mx-5 my-7 text-xl">Hello, {getFirstName(name)}. Are any of these of interest to you?</h1>
+        <article className="bg-amber-100 border border-black border-solid w-[90%] h-[335px] p-5 rounded-md flex gap-10 overflow-x-auto overflow-y-hidden" style={{scrollbarColor:"#8b8b8b #fef3c6"}}>
           {foodPack.map((value, index) => (
-            <div key={index} className="basis-full flex flex-col justify-center" onClick={() => { setFindex(index) }}>
+            <div key={index} className="w-[calc((100%-120px)/4)] flex flex-col justify-center shrink-0" onClick={() => { setFindex(index) }}>
               <figure className="relative border border-black border-solid w-full h-[70%] flex items-end rounded-md overflow-hidden">
                 <img className="absolute w-full h-full object-cover " src="./template.jpeg" alt="" />
                 <div className="w-full z-50 bg-linear-to-b from-[rgba(0,0,0,0)] to-[rgba(0,0,0,6)]">
@@ -87,14 +96,13 @@ export default function Home() {
             </div>
           </section>
           <section className="basis-full flex flex-col ">
-            <h2 className="text-2xl font-bold">Category</h2>
+            <h2 className="text-2xl font-bold">Category: {foodPack[Findex].category}</h2>
             <div className="mt-4 pl-2 overflow-x-hidden overflow-y-auto">
               <h3>Ingredients:</h3>
               <ul className="list-disc ps-5">
                 {foodPack[Findex].ingredients.map((value, index) => <li key={index}>{value}</li>)}
               </ul>
             </div>
-
           </section>
         </article>
       </main>
